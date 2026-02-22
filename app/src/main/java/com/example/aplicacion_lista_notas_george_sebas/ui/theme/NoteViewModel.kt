c32242b2d4d16c99651fc903d054ab920421f2fd
George <mr.georgemanea@gmail.com>
2026-02-22 21:49:16 +0100

    Initial commit

---
+++ app/src/main/java/com/example/aplicacion_lista_notas_george_sebas/ui/theme/NoteViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.aplicacion_lista_notas_george_sebas.ui.theme
+
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+
+class NoteViewModel : ViewModel() {
+    private val _notes = mutableStateListOf<String>()
+    val notes: List<String> = _notes
+
+    fun addNote(note: String) {
+        if (note.isNotBlank()) {
+            _notes.add(note)
+        }
+    }
+
+    fun removeNote(note: String) {
+        _notes.remove(note)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Aplicacion_lista_notas_George_Sebas"
+include(":app")
+ 
