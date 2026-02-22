3e2ba9a51ab4194cc3b5e2455992cb00e537a1eb
Sebastian Mertic <alumer2408@ieselcaminas.org>
2026-02-22 22:00:27 +0100

    Main modificado, añadiendo dentro de column, Text(), Spacer(), OutlinedTextField()

--- app/src/main/java/com/example/aplicacion_lista_notas_george_sebas/MainActivity.kt
+++ app/src/main/java/com/example/aplicacion_lista_notas_george_sebas/MainActivity.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.aplicacion_lista_notas_george_sebas.ui.NoteViewModel
 import com.example.aplicacion_lista_notas_george_sebas.ui.theme.Aplicacion_lista_notas_George_SebasTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +32,31 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Aplicacion_lista_notas_George_SebasTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
-    }
-}
+
+
+
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun NoteScreen(viewModel: NoteViewModel) {
+    var text by remember { mutableStateOf("") }
+    val context = LocalContext.current
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Aplicacion_lista_notas_George_SebasTheme {
-        Greeting("Android")
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text(
+            text = stringResource(id = R.string.notes_list_title),
+            style = MaterialTheme.typography.headlineMedium
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text(stringResource(id = R.string.note_hint)) },
+            modifier = Modifier.fillMaxWidth()
+        )
     }
+
 }
